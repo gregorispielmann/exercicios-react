b6acca1d6bf7bd420764e4b96f9d9ad05ea5862b
Marcos Henrique Stuber Dreveck <ja31101@malwee.com.br>
2020-06-26 11:59:54 -0300

    inicio todo com redux

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
-export default (props) => {
+import { connect } from 'react-redux'
+
+const TodoForm = props => {
   const keyHandler = (e) => {
     if (e.key === "Enter")
       e.shiftKey ? props.handleSearch() : props.handleAdd();
@@ -30,3 +32,11 @@ export default (props) => {
     </div>
   );
 };
+
+const mapStateToProps = state => {
+  return{ 
+    description: state.todo.description
+  }
+}
+
+export default connect(mapStateToProps)(TodoForm) 
